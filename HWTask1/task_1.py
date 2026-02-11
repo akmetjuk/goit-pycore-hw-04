a2138a51cf8ade9d092d96a0e785e99b49ac89fe
akmetjuk <akmetjuk@gmail.com>
2026-02-11 08:55:36 +0200

    - first task

--- HWTask1/task_1.py
+++ HWTask1/task_1.py
@@ -10,18 +10,23 @@ def total_salary(path: str) -> tuple[int, int]:
         tuple: Загальна сума заробітної плати та середня сума заробітної плати
 
     Raises:
-        ValueError: Якщо формат даних у файлі неправильний або якщо файл не існує
+        FileNotFoundError: Якщо файл за вказаним шляхом не знайдено
     """
-    with open(path, 'r', encoding="utf-8") as file:
-        lines = file.readlines()
-        total = 0
-        for line in lines:
-            parts = line.split(',')
-            if len(parts) >= 2:
-                try:
-                    salary = int(parts[1].strip())
-                    total += salary
-                except ValueError:
-                    continue
-        average_salary = total / len(lines) if lines else 0
-        return total, average_salary
+    try:
+        with open(path, 'r', encoding="utf-8") as file:
+            lines = file.readlines()
+            total = 0
+            for line in lines:
+                parts = line.split(',')
+                if len(parts) >= 2:
+                    try:
+                        salary = int(parts[1].strip())
+                        total += salary if salary > 0 else 0
+                    except ValueError:
+                        continue
+            average_salary = total / len(lines) if lines else 0
+            return total, average_salary
+    except UnicodeDecodeError:
+        raise
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Файл за шляхом {path} не знайдено.")
--- main.py
+++ main.py
@@ -4,5 +4,16 @@ import pathlib
 
 if __name__ == "__main__":
     current_dir = pathlib.Path(__file__).parent
-    total, average = total_salary(current_dir / "HWTask1" / "salary.txt")
+
+    print(f"{'=' * 8} task #1 checking")
+    total, average = total_salary(current_dir / "HWTask1" / "sallary.txt")
     print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
+    try:
+        total_salary(current_dir / "HWTask1" / "non_existent_sallary_file.txt")
+    except Exception as e:
+        print(e)
+
+    try:
+        total_salary(current_dir / "HWTask1" / "sallary_nonUTF-8.txt")
+    except Exception as e:
+        print(e)
